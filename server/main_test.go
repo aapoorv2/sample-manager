39f8f9f3e44c342c3430f2735beed99cb8d02423
aapoorv2 <abhyudaya.a_ftc@external.swiggy.in>
2024-03-19 12:51:02 +0530

    Add tests for getting a sample ID

--- server/main_test.go
+++ server/main_test.go
@@ -8,6 +8,7 @@ import (
 	pb "sample-manager/proto"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -15,7 +16,9 @@ import (
 	"gorm.io/gorm"
 )
 
-func TestCreatingAMapping(t *testing.T) {
+
+
+func TestGettingASampleID(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	assert.Nil(t, err, "Error creating mock db: %v", err)
 
@@ -31,57 +34,49 @@ func TestCreatingAMapping(t *testing.T) {
 
 	type args struct {
 		ctx context.Context
-		req *pb.CreateRequest
+		req *pb.GetRequest
 	}
 	tests := []struct {
 		name    string
 		args    args
 		rows    func()
-		want    *pb.CreateResponse
+		want    *pb.GetResponse
 		wantErr bool
 		errorCode codes.Code
 	}{
 		{
-			name: "Creating a mapping - Expect Success",
+			name: "Getting a sample ID - Expect Success",
 			args: args{
 				ctx: context.Background(),
-				req: &pb.CreateRequest{
-					Segments: []string{"test_segment"},
+				req: &pb.GetRequest{
+					Clm: []string{"test_segment1", "test_segment2"},
 					ItemId: "test_item_id",
-					SampleItemId: "test_sample_item_id",
 				},
 			},
 			rows: func() {
-				mock.ExpectBegin()
-				rows := sqlmock.NewRows([]string{
-					"id",
-					"segments",
-					"item_id",
-					"sample_item_id",
-				}).AddRow(1, "", "", "")
-				mock.ExpectQuery("INSERT").WillReturnRows(rows)
-				mock.ExpectCommit()
+				mock.ExpectQuery("SELECT").WillReturnRows(sqlmock.NewRows([]string{"id", "sample_item_id", "item_id", "segments"}).
+					AddRow(1, "1", "test_item_id", pq.StringArray{"test_segment1", "test_segment2"}))
 			},
-			want: &pb.CreateResponse{
-				Message: constants.CREATE_MAPPING_SUCCESS,
+			want: &pb.GetResponse{
+				SampleItemId: "1",
 			},
 			wantErr: false,
 		},
 		{
-			name: "Creating a mapping - Expect Error",
+			name: "Getting a sample ID when mapping is doesn't exist - Expect Error",
 			args: args{
 				ctx: context.Background(),
-				req: &pb.CreateRequest{
-					Segments: []string{"test_segment"},
+				req: &pb.GetRequest{
+					Clm: []string{"test_segment1", "test_segment2"},
 					ItemId: "test_item_id",
-					SampleItemId: "test_sample_item_id",
 				},
 			},
 			rows: func() {
+				mock.ExpectQuery("SELECT").WillReturnRows(sqlmock.NewRows([]string{}))
 			},
-			want:    nil,
+			want: nil,
 			wantErr: true,
-			errorCode: codes.Unknown,
+			errorCode: codes.Unavailable,
 		},
 	}
 
@@ -90,22 +85,18 @@ func TestCreatingAMapping(t *testing.T) {
 			tt.rows()
 			server := &Server{DB: gormDb} 
 
-			got, err := server.CreateMapping(tt.args.ctx, tt.args.req)
+			got, err := server.GetSampleId(tt.args.ctx, tt.args.req)
 
 			if (err != nil) != tt.wantErr {
-				t.Fatalf("CreateMapping() error = %v, wantErr %v", err, tt.wantErr)
+				t.Fatalf("GetSampleId() error = %v, wantErr %v", err, tt.wantErr)
 			}
 			if tt.wantErr {
 				statusErr, ok := status.FromError(err)
 				assert.True(t, ok, "Expected gRPC status error")
 				assert.Equalf(t, tt.errorCode, statusErr.Code(), "Expected %v error", tt.errorCode)
 			} else {
-				assert.Equalf(t, tt.want, got, "CreateMapping(%v, %v)", tt.args.ctx, tt.args.req)
+				assert.Equalf(t, tt.want, got, "GetSampleId(%v, %v)", tt.args.ctx, tt.args.req)
 			}
 		})
 	}
-}
-
-func TestGettingASampleID(t *testing.T) {
-	
 }
